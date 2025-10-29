64ae6f79c4c3c63111201041857a06c8b1f4ac81
Vyacheslav <volkovs976@rambler.ru>
2025-10-29 16:18:14 +0300

    Lesson_04

--- Lesson_06/Rename_button.py
+++ Lesson_06/Rename_button.py
@@ -3,9 +3,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("http://uitestingplayground.com/textinput")
