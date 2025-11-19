3b0993c274668cd30da5d3ec97dcfab890ad6e79
Vyacheslav <volkovs976@rambler.ru>
2025-11-19 17:36:24 +0300

    Lesson_09

--- Lesson_09/test_lesson_09_db.py
+++ Lesson_09/test_lesson_09_db.py
@@ -10,95 +10,60 @@ subject_id = '6'
 
 
 def test_create_new_student():
-    try:
-        db.create(user_id, level, education_form, subject_id)
+    db.create(user_id, level, education_form, subject_id)
+    created_student = db.get_student_by_id(user_id)
 
-        created_student = db.get_student_by_id(user_id)
+    assert len(created_student) > 0, "Запись о студенте не найдена."
+    assert created_student[0]['user_id'] == int(user_id)
+    assert created_student[0]['level'] == level
+    assert created_student[0]['education_form'] == education_form
+    assert created_student[0]['subject_id'] == int(subject_id)
 
-        if len(created_student) == 0:
-            print("Запись о студенте не найдена.")
-        else:
-            assert created_student[0]['user_id'] == int(user_id)
-            assert created_student[0]['level'] == level
-            assert created_student[0]['education_form'] == education_form
-            assert created_student[0]['subject_id'] == int(subject_id)
-            print("Запись о студенте успешно создана. Все проверки пройдены")
-
-        print("=== test_create_new_student УСПЕШНО завершен ===\n")
-
-    except Exception as e:
-        print(f"Тест завершился с ошибкой: {str(e)}")
-
-    finally:
-        db.delete(user_id)
-        result = db.get_student_by_id(user_id)
-        assert len(result) == 0
+    db.delete(user_id)
+    result = db.get_student_by_id(user_id)
+    assert len(result) == 0
 
 
 def test_change_education_form():
-    try:
-        db.create(user_id, level, education_form, subject_id)
+    db.create(user_id, level, education_form, subject_id)
+    created_student = db.get_student_by_id(user_id)
 
-        created_student = db.get_student_by_id(user_id)
+    assert len(created_student) > 0, "Запись о студенте не найдена."
+    assert created_student[0]['user_id'] == int(user_id)
+    assert created_student[0]['level'] == level
+    assert created_student[0]['education_form'] == education_form
+    assert created_student[0]['subject_id'] == int(subject_id)
 
-        if len(created_student) == 0:
-            print("Запись о студенте не найдена.")
-        else:
-            assert created_student[0]['user_id'] == int(user_id)
-            assert created_student[0]['level'] == level
-            assert created_student[0]['education_form'] == education_form
-            assert created_student[0]['subject_id'] == int(subject_id)
-            print("Запись о студенте успешно создана. Проверки пройдены.")
+    db.change_education_form(user_id, new_education_form)
+    changed_student = db.get_student_by_id(user_id)
 
-            db.change_education_form(user_id, new_education_form)
-            changed_student = db.get_student_by_id(user_id)
+    assert len(changed_student) > 0, (
+        "Запись о студенте не найдена"
+        " после изменения формы образования."
+    )
+    assert changed_student[0]['user_id'] == int(user_id)
+    assert changed_student[0]['level'] == level
+    assert (changed_student[0]['education_form'] ==
+            new_education_form)
+    assert changed_student[0]['subject_id'] == int(subject_id)
 
-            if len(changed_student) == 0:
-                print("Запись о студенте не найдена "
-                      "после изменения формы образования.")
-            else:
-                assert changed_student[0]['user_id'] == int(user_id)
-                assert changed_student[0]['level'] == level
-                assert (changed_student[0]['education_form'] ==
-                        new_education_form)
-                assert changed_student[0]['subject_id'] == int(subject_id)
-                print("Форма обучения изменена. "
-                      "Проверки после изменения пройдены.")
-
-        print("=== test_change_education_form УСПЕШНО завершен ===\n")
-
-    except Exception as e:
-        print(f"Тест завершился с ошибкой: {str(e)}")
-
-    finally:
-        db.delete(user_id)
-        rows = db.get_student_by_id(user_id)
-        assert len(rows) == 0
+    db.delete(user_id)
+    rows = db.get_student_by_id(user_id)
+    assert len(rows) == 0
 
 
 def test_delete():
-    try:
-        db.create(user_id, level, education_form, subject_id)
+    db.create(user_id, level, education_form, subject_id)
+    created_student = db.get_student_by_id(user_id)
 
-        created_student = db.get_student_by_id(user_id)
+    assert len(created_student) > 0, "Запись о студенте не найдена."
+    assert created_student[0]['user_id'] == int(user_id)
+    assert created_student[0]['level'] == level
+    assert created_student[0]['education_form'] == education_form
+    assert created_student[0]['subject_id'] == int(subject_id)
 
-        if len(created_student) == 0:
-            print("Запись о студенте не найдена.")
-        else:
-            assert created_student[0]['user_id'] == int(user_id)
-            assert created_student[0]['level'] == level
-            assert created_student[0]['education_form'] == education_form
-            assert created_student[0]['subject_id'] == int(subject_id)
-            print("Запись о студенте успешно создана. Проверки пройдены.")
+    deleted_student = db.delete(user_id)
+    assert deleted_student["detail"] == "Student successfully deleted"
 
-            deleted_student = db.delete(user_id)
-            assert deleted_student["detail"] == "Student successfully deleted"
-
-            rows = db.get_student_by_id(user_id)
-            assert len(rows) == 0
-            print("Запись о студенте успешно удалена. Проверки пройдены.")
-
-        print("=== test_delete УСПЕШНО завершен ===\n")
-
-    except Exception as e:
-        print(f"Тест завершился с ошибкой: {str(e)}")
+    rows = db.get_student_by_id(user_id)
+    assert len(rows) == 0
