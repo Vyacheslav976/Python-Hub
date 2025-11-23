3cc41d2b56a3410a5d2e1f1ffd3a3fcdefc2f60d
Vyacheslav976 <volkovs976@rambler.ru>
2025-11-23 13:22:58 +0300

    Delete lesson_02 directory

--- lesson_02/lesson_2_task_2.py
+++
@@ -1,8 +0,0 @@
-def is_yesr_leap(year):
-    if (year % 4 == 0):
-        print(f"Год {year}: - True")
-    else:
-        print(f"Год {year}: - False")
-
-
-is_yesr_leap(int(input("Введите год: ")))
